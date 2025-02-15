6f1af892915a8a572c79b71951d26986f59445fd
Alok Rai <101451098+AlokRai123@users.noreply.github.com>
2025-02-15 23:41:07 +0530

    ui whyChooseUs

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -22,9 +22,9 @@ function Navbar({ className }: { className?: string }) {
          <MenuItem setActive={setActive} active={active} item="Courses">
           <div className="flex flex-col space-y-4 text-sm">
           <HoveredLink href="/courses">All Courses</HoveredLink>
-         <HoveredLink href="/courses">Basic Music Theory</HoveredLink>
-         <HoveredLink href="/courses">Advanced Composition</HoveredLink>
-         <HoveredLink href="/courses">Songwriting</HoveredLink>
+          <HoveredLink href="/courses">Basic Music Theory</HoveredLink>
+          <HoveredLink href="/courses">Advanced Composition</HoveredLink>
+          <HoveredLink href="/courses">Songwriting</HoveredLink>
           </div>
 
          </MenuItem>
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,4 +1,9 @@
 import type { Config } from "tailwindcss";
+const defaultTheme = require("tailwindcss/defaultTheme");
+const colors = require("tailwindcss/colors");
+const {
+  default: flattenColorPalette,
+} = require("tailwindcss/lib/util/flattenColorPalette");
 
 
 export default {
@@ -32,7 +37,19 @@ export default {
     },
   },
   plugins: [
-   
+    addVariablesForColors,
   ],
 } satisfies Config;
+
+
+function addVariablesForColors({ addBase, theme }: any) {
+  let allColors = flattenColorPalette(theme("colors"));
+  let newVars = Object.fromEntries(
+    Object.entries(allColors).map(([key, val]) => [`--${key}`, val])
+  );
+ 
+  addBase({
+    ":root": newVars,
+  });
+}
 
