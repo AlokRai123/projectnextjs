e123e4d5aae9add015015f628c7cd7ff38b02281
Alok Rai <101451098+AlokRai123@users.noreply.github.com>
2025-02-17 23:50:30 +0530

    Making Instructor Section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,18 +4,19 @@ import TestimonialCards from "../components/testimonialCards";
 
 import WhyChooseUs from "@/components/WhyChooseUs";
 import UpcomingWebinar from "@/components/UpcomingWebinar";
+import Instructors from "@/components/Instructor";
 
 
 export default function Home() {
   return (
   <main className="min-h-screen bg-black/[0.96] antialiased bg-grid-white/[0.02]">
 
-   <h1 className="text-3xl text-center">Home Page</h1>
    <HeroSection/>
    <FeaturedCourses/>
    <WhyChooseUs/>
    <TestimonialCards/>
    <UpcomingWebinar/>
+   <Instructors/>
   </main>
   );
 }
