43e063a97f40396a7b5371b5e546ed815a0c79c6
Alok Rai <101451098+AlokRai123@users.noreply.github.com>
2025-02-17 23:11:52 +0530

    Card Hover

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,7 @@ import HeroSection from "@/components/HeroSection";
 import TestimonialCards from "../components/testimonialCards";
 
 import WhyChooseUs from "@/components/WhyChooseUs";
+import UpcomingWebinar from "@/components/UpcomingWebinar";
 
 
 export default function Home() {
@@ -14,6 +15,7 @@ export default function Home() {
    <FeaturedCourses/>
    <WhyChooseUs/>
    <TestimonialCards/>
+   <UpcomingWebinar/>
   </main>
   );
 }
